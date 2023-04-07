b057a050ed2528b0c0f39308e40d4e8ceec8c2f1
Danil M <malaxoff.dan@gmail.com>
2023-04-08 00:11:17 +0700

    refactoring

---
+++ internal/handler/auth.go
@@ -0,0 +1,18 @@
+package handler
+
+import "net/http"
+
+func (h *Handler) HandleSignUp() http.HandlerFunc {
+
+	return nil
+}
+
+func (h *Handler) HandleSignIn() http.HandlerFunc {
+
+	return nil
+}
+
+func (h *Handler) HandleGetUserById() http.HandlerFunc {
+
+	return nil
+}
